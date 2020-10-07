2cccc9725a08f6af14c9d85459d839edee3a6541
Alexander Klandorf <AlexanderKlandorf@MacBook-Air.local>
2020-10-06 20:21:52 -0400

    commit on master

---
+++ README.md
@@ -0,0 +1,3 @@
+# website
+lol lol lol
+
---
+++ resumescript.js
@@ -0,0 +1,350 @@
+var images = [
+    [
+      {id: 'path5200', d: 'm 67,23 0,14.1 -14.1,-14.1 z', fill: '#eec16e'},
+      {id: 'path4604', d: 'm 52.987578,37.012422 0,-14.012422 14.012422,14.012422 z', fill: '#eec16e'},
+     {id: 'path0003', d: 'm 50 125 L 100 125 L 50 75 z', fill: '#393939'},
+      {id: '#path0004', d: 'm 100 75 L 100 125 L 50 75 z', fill: '#393939'},
+      {id: '#path0005', d: 'm 150 75 L 150 125 L 100 75 z', fill: '#393939'},
+      {id: '#path0006', d: 'm 100 125 L 150 185 L 100 75 z', fill: '#393939'},
+      {id: '#path0007', d: 'm 150 125 L 150 185 L 100 75 z', fill: '#393939'},
+      {id: '#path0008', d: 'm 150 75 L 150 185 L 200 150 z', fill: '#393939'},
+      {id: '#path0009', d: 'm 150 185 L 160 200 L 200 150 z', fill: '#393939'},
+      {id: '#path0010', d: 'm 150 185 L 200 260 L 200 150 z', fill: '#393939'},
+      {id: '#path0011', d: 'm 200 260 L 200 150 L 260 150 z', fill: '#393939'},
+      {id: '#path0012', d: 'm 200 150 L 260 75 L 260 150 z', fill: '#393939'},
+      {id: '#path0013', d: 'm 260 75 L 260 150 L 320 150 z', fill: '#393939'},
+      {id: '#path0014', d: 'm 260 150 L 320 260 L 320 150 z', fill: '#393939'}, 
+      {id: '#path0015', d: 'm 320 260 L 320 150 L 360 150 z', fill: '#393939'},
+      {id: '#path0016', d: 'm 320 150 L 380 75 L 360 150 z', fill: '#393939'},
+      {id: '#path0017', d: 'm 320 260 L 380 75 L 380 150 z', fill: '#393939'},
+      {id: '#path0018', d: 'm 380 150 L 380 75 L 420 75 z', fill: '#393939'},
+      {id: '#path0019', d: 'm 380 125 L 400 125 L 420 75 z', fill: '#393939'},
+      {id: '#path0020', d: 'm 400 125 L 450 125 L 420 75 z', fill: '#393939'},
+      {id: '#path0021', d: 'm 450 125 L 450 75 L 400 75 z', fill: '#393939'},
+      {id: '#path0022', d: 'm 215 280 L 260 200 L 260 280 z', fill: '#393939'},
+      {id: '#path0023', d: 'm 260 200 L 260 300 L 320 300 z', fill: '#393939'},
+      {id: '#path0024', d: 'm 215 280 L 260 350 L 260 280 z', fill: '#393939'},
+      {id: '#path0025', d: 'm 260 300 L 260 350 L 320 445 z', fill: '#393939'},
+      {id: '#path0026', d: 'm 260 300 L 320 300 L 320 445 z', fill: '#393939'},
+      {id: '#path0027', d: 'm 320 300 L 380 300 L 320 445 z', fill: '#393939'},
+      {id: '#path0028', d: 'm 320 300 L 380 200 L 380 300 z', fill: '#393939'},
+      {id: '#path0029', d: 'm 380 200 L 420 200 L 380 300 z', fill: '#393939'},
+      {id: '#path0030', d: 'm 420 200 L 450 240 L 390 240 z', fill: '#393939'},
+      {id: '#path0031', d: 'm 450 200 L 450 240 L 390 200 z', fill: '#393939'},
+
+    ],
+    [
+      {id: '#path5200', d: 'm 82.9,37.012346 14.1,0.08765 0,-14.2 z', fill: '#c63f2b'},
+      {id: '#path4604', d: 'm 82.987578,22.987654 0,14.024692 14.012422,-14.024692 z', fill: '#c63f2b'},
+       {id: '#path0003', d: 'm 30 125 L 100 125 L 50 89 z', fill: '#393939'},
+       {id: '#path0004', d: 'm 100 75 L 100 300 L 50 75 z', fill: '#393939'},
+       {id: '#path0005', d: 'm 150 75 L 150 125 L 100 75 z', fill: '#393939'},
+       {id: '#path0006', d: 'm 100 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0007', d: 'm 150 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0008', d: 'm 150 75 L 150 185 L 200 150 z', fill: '#393939'},
+       {id: '#path0009', d: 'm 150 185 L 160 200 L 200 150 z', fill: '#393939'},
+       {id: '#path0010', d: 'm 150 185 L 200 260 L 200 100 z', fill: '#393939'},
+       {id: '#path0011', d: 'm 200 260 L 200 150 L 260 150 z', fill: '#393939'},
+       {id: '#path0012', d: 'm 200 150 L 260 75 L 260 150 z', fill: '#393939'},
+       {id: '#path0013', d: 'm 260 75 L 260 150 L 320 150 z', fill: '#393939'},
+       {id: '#path0014', d: 'm 260 150 L 320 260 L 320 150 z', fill: '#393939'}, 
+       {id: '#path0015', d: 'm 320 260 L 320 150 L 360 150 z', fill: '#393939'},
+       {id: '#path0016', d: 'm 320 150 L 380 75 L 360 150 z', fill: '#393939'},
+       {id: '#path0017', d: 'm 320 260 L 380 75 L 380 150 z', fill: '#393939'},
+       {id: '#path0018', d: 'm 380 150 L 380 75 L 420 75 z', fill: '#393939'},
+       {id: '#path0019', d: 'm 380 125 L 400 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0020', d: 'm 400 125 L 450 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0021', d: 'm 450 125 L 450 75 L 400 75 z', fill: '#393939'},
+       {id: '#path0022', d: 'm 215 280 L 260 200 L 260 280 z', fill: '#393939'},
+       {id: '#path0023', d: 'm 260 200 L 260 300 L 320 300 z', fill: '#393939'},
+       {id: '#path0024', d: 'm 215 280 L 260 350 L 260 280 z', fill: '#393939'},
+       {id: '#path0025', d: 'm 260 300 L 260 350 L 320 445 z', fill: '#393939'},
+       {id: '#path0026', d: 'm 260 300 L 320 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0027', d: 'm 320 300 L 380 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0028', d: 'm 320 300 L 380 100 L 380 300 z', fill: '#393939'},
+       {id: '#path0029', d: 'm 380 200 L 420 200 L 380 300 z', fill: '#393939'},
+       {id: '#path0030', d: 'm 420 200 L 450 240 L 390 240 z', fill: '#393939'},
+       {id: '#path0031', d: 'm 450 200 L 450 240 L 390 200 z', fill: '#393939'},
+ 
+     ],
+     [
+      {id: '#path4604', d: 'm 112.98758,37.012422 0,-14.012422 14.01242,14.012422 z', fill: '#8d5429'},
+      {id: '#path5200', d: 'm 127,23 0,14.1 -14.1,-14.1 z', fill: '#8d5429'},
+      {id: '#path0003', d: 'm 50 125 L 100 125 L 50 75 z', fill: '#393939'},
+       {id: '#path0004', d: 'm 100 75 L 100 125 L 50 75 z', fill: '#393939'},
+       {id: '#path0005', d: 'm 150 75 L 150 125 L 100 75 z', fill: '#393939'},
+       {id: '#path0006', d: 'm 100 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0007', d: 'm 150 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0008', d: 'm 150 75 L 150 185 L 200 150 z', fill: '#393939'},
+       {id: '#path0009', d: 'm 150 185 L 160 200 L 200 150 z', fill: '#393939'},
+       {id: '#path0010', d: 'm 150 185 L 200 260 L 200 150 z', fill: '#393939'},
+       {id: '#path0011', d: 'm 200 260 L 200 150 L 260 150 z', fill: '#393939'},
+       {id: '#path0012', d: 'm 200 150 L 260 75 L 260 150 z', fill: '#393939'},
+       {id: '#path0013', d: 'm 260 75 L 260 150 L 320 150 z', fill: '#393939'},
+       {id: '#path0014', d: 'm 260 150 L 320 260 L 320 150 z', fill: '#393939'}, 
+       {id: '#path0015', d: 'm 320 260 L 320 150 L 360 150 z', fill: '#393939'},
+       {id: '#path0016', d: 'm 320 150 L 380 75 L 360 150 z', fill: '#393939'},
+       {id: '#path0017', d: 'm 320 260 L 380 75 L 380 150 z', fill: '#393939'},
+       {id: '#path0018', d: 'm 380 150 L 380 75 L 420 75 z', fill: '#393939'},
+       {id: '#path0019', d: 'm 380 125 L 400 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0020', d: 'm 400 125 L 450 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0021', d: 'm 450 125 L 450 75 L 400 75 z', fill: '#393939'},
+       {id: '#path0022', d: 'm 215 280 L 260 200 L 260 280 z', fill: '#393939'},
+       {id: '#path0023', d: 'm 260 200 L 260 300 L 320 300 z', fill: '#393939'},
+       {id: '#path0024', d: 'm 215 280 L 260 350 L 260 280 z', fill: '#393939'},
+       {id: '#path0025', d: 'm 260 300 L 260 350 L 320 445 z', fill: '#393939'},
+       {id: '#path0026', d: 'm 260 300 L 320 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0027', d: 'm 320 300 L 380 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0028', d: 'm 320 300 L 380 200 L 380 300 z', fill: '#393939'},
+       {id: '#path0029', d: 'm 380 200 L 420 200 L 380 300 z', fill: '#393939'},
+       {id: '#path0030', d: 'm 420 200 L 450 240 L 390 240 z', fill: '#393939'},
+       {id: '#path0031', d: 'm 450 200 L 450 240 L 390 200 z', fill: '#393939'},
+ 
+     ],
+     [
+      
+      {id: '#path4604', d: 'm 142.98758,37.012346 14.01242,-14.024692 -14.01242,0 z', fill: '#59908d'},
+      {id: '#path5200', d: 'm 142.9,37.012346 14.1,-14.112346 0,14.2 z', fill: '#59908d'},{id: '#path0003', d: 'm 50 125 L 100 125 L 50 75 z', fill: '#393939'},
+       {id: '#path0004', d: 'm 100 75 L 100 125 L 50 75 z', fill: '#393939'},
+       {id: '#path0005', d: 'm 150 75 L 150 125 L 100 75 z', fill: '#393939'},
+       {id: '#path0006', d: 'm 100 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0007', d: 'm 150 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0008', d: 'm 150 75 L 150 185 L 200 150 z', fill: '#393939'},
+       {id: '#path0009', d: 'm 150 185 L 160 200 L 200 150 z', fill: '#393939'},
+       {id: '#path0010', d: 'm 150 185 L 200 260 L 200 150 z', fill: '#393939'},
+       {id: '#path0011', d: 'm 200 260 L 200 150 L 260 150 z', fill: '#393939'},
+       {id: '#path0012', d: 'm 200 150 L 260 75 L 260 150 z', fill: '#393939'},
+       {id: '#path0013', d: 'm 260 75 L 260 150 L 320 150 z', fill: '#393939'},
+       {id: '#path0014', d: 'm 260 150 L 320 260 L 320 150 z', fill: '#393939'}, 
+       {id: '#path0015', d: 'm 320 260 L 320 150 L 360 150 z', fill: '#393939'},
+       {id: '#path0016', d: 'm 320 150 L 380 75 L 360 150 z', fill: '#393939'},
+       {id: '#path0017', d: 'm 320 260 L 380 75 L 380 150 z', fill: '#393939'},
+       {id: '#path0018', d: 'm 380 150 L 380 75 L 420 75 z', fill: '#393939'},
+       {id: '#path0019', d: 'm 380 125 L 400 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0020', d: 'm 400 125 L 450 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0021', d: 'm 450 125 L 450 75 L 400 75 z', fill: '#393939'},
+       {id: '#path0022', d: 'm 215 280 L 260 200 L 260 280 z', fill: '#393939'},
+       {id: '#path0023', d: 'm 260 200 L 260 300 L 320 300 z', fill: '#393939'},
+       {id: '#path0024', d: 'm 215 280 L 260 350 L 260 280 z', fill: '#393939'},
+       {id: '#path0025', d: 'm 260 300 L 260 350 L 320 445 z', fill: '#393939'},
+       {id: '#path0026', d: 'm 260 300 L 320 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0027', d: 'm 320 300 L 380 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0028', d: 'm 320 300 L 380 200 L 380 300 z', fill: '#393939'},
+       {id: '#path0029', d: 'm 380 200 L 420 200 L 380 300 z', fill: '#393939'},
+       {id: '#path0030', d: 'm 420 200 L 450 240 L 390 240 z', fill: '#393939'},
+       {id: '#path0031', d: 'm 450 200 L 450 240 L 390 200 z', fill: '#393939'},
+ 
+     ],
+     [
+      
+      {id: '#path4604', d: 'm 186.99999,23.000015 0,14.099975 -14.09998,-14.099975 z', fill: '#606873'},
+      {id: '#path5200', d: 'm 186.99999,37.012412 -14.0124,0 0,-14.012397 z', fill: '#606873'},{id: '#path0003', d: 'm 50 125 L 100 125 L 50 75 z', fill: '#393939'},
+       {id: '#path0004', d: 'm 100 75 L 100 125 L 50 75 z', fill: '#393939'},
+       {id: '#path0005', d: 'm 150 75 L 150 125 L 100 75 z', fill: '#393939'},
+       {id: '#path0006', d: 'm 100 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0007', d: 'm 150 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0008', d: 'm 150 75 L 150 185 L 200 150 z', fill: '#393939'},
+       {id: '#path0009', d: 'm 150 185 L 160 200 L 200 150 z', fill: '#393939'},
+       {id: '#path0010', d: 'm 150 185 L 200 260 L 200 150 z', fill: '#393939'},
+       {id: '#path0011', d: 'm 200 260 L 200 150 L 260 150 z', fill: '#393939'},
+       {id: '#path0012', d: 'm 200 150 L 260 75 L 260 150 z', fill: '#393939'},
+       {id: '#path0013', d: 'm 260 75 L 260 150 L 320 150 z', fill: '#393939'},
+       {id: '#path0014', d: 'm 260 150 L 320 260 L 320 150 z', fill: '#393939'}, 
+       {id: '#path0015', d: 'm 320 260 L 320 150 L 360 150 z', fill: '#393939'},
+       {id: '#path0016', d: 'm 320 150 L 380 75 L 360 150 z', fill: '#393939'},
+       {id: '#path0017', d: 'm 320 260 L 380 75 L 380 150 z', fill: '#393939'},
+       {id: '#path0018', d: 'm 380 150 L 380 75 L 420 75 z', fill: '#393939'},
+       {id: '#path0019', d: 'm 380 125 L 400 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0020', d: 'm 400 125 L 450 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0021', d: 'm 450 125 L 450 75 L 400 75 z', fill: '#393939'},
+       {id: '#path0022', d: 'm 215 280 L 260 200 L 260 280 z', fill: '#393939'},
+       {id: '#path0023', d: 'm 260 200 L 260 300 L 320 300 z', fill: '#393939'},
+       {id: '#path0024', d: 'm 215 280 L 260 350 L 260 280 z', fill: '#393939'},
+       {id: '#path0025', d: 'm 260 300 L 260 350 L 320 445 z', fill: '#393939'},
+       {id: '#path0026', d: 'm 260 300 L 320 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0027', d: 'm 320 300 L 380 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0028', d: 'm 320 300 L 380 200 L 380 300 z', fill: '#393939'},
+       {id: '#path0029', d: 'm 380 200 L 420 200 L 380 300 z', fill: '#393939'},
+       {id: '#path0030', d: 'm 420 200 L 450 240 L 390 240 z', fill: '#393939'},
+       {id: '#path0031', d: 'm 450 200 L 450 240 L 390 200 z', fill: '#393939'},
+ 
+     ],
+     [
+      {id: '#path0003', d: 'm 50 125 L 100 125 L 50 75 z', fill: '#393939'},
+       {id: '#path0004', d: 'm 100 75 L 100 125 L 50 75 z', fill: '#393939'},
+       {id: '#path0005', d: 'm 150 75 L 150 125 L 100 75 z', fill: '#393939'},
+       {id: '#path0006', d: 'm 100 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0007', d: 'm 150 125 L 150 185 L 100 75 z', fill: '#393939'},
+       {id: '#path0008', d: 'm 150 75 L 150 185 L 200 150 z', fill: '#393939'},
+       {id: '#path0009', d: 'm 150 185 L 160 200 L 200 150 z', fill: '#393939'},
+       {id: '#path0010', d: 'm 150 185 L 200 260 L 200 150 z', fill: '#393939'},
+       {id: '#path0011', d: 'm 200 260 L 200 150 L 260 150 z', fill: '#393939'},
+       {id: '#path0012', d: 'm 200 150 L 260 75 L 260 150 z', fill: '#393939'},
+       {id: '#path0013', d: 'm 260 75 L 260 150 L 320 150 z', fill: '#393939'},
+       {id: '#path0014', d: 'm 260 150 L 320 260 L 320 150 z', fill: '#393939'}, 
+       {id: '#path0015', d: 'm 320 260 L 320 150 L 360 150 z', fill: '#393939'},
+       {id: '#path0016', d: 'm 320 150 L 380 75 L 360 150 z', fill: '#393939'},
+       {id: '#path0017', d: 'm 320 260 L 380 75 L 380 150 z', fill: '#393939'},
+       {id: '#path0018', d: 'm 380 150 L 380 75 L 420 75 z', fill: '#393939'},
+       {id: '#path0019', d: 'm 380 125 L 400 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0020', d: 'm 400 125 L 450 125 L 420 75 z', fill: '#393939'},
+       {id: '#path0021', d: 'm 450 125 L 450 75 L 400 75 z', fill: '#393939'},
+       {id: '#path0022', d: 'm 215 280 L 260 200 L 260 280 z', fill: '#393939'},
+       {id: '#path0023', d: 'm 260 200 L 260 300 L 320 300 z', fill: '#393939'},
+       {id: '#path0024', d: 'm 215 280 L 260 350 L 260 280 z', fill: '#393939'},
+       {id: '#path0025', d: 'm 260 300 L 260 350 L 320 445 z', fill: '#393939'},
+       {id: '#path0026', d: 'm 260 300 L 320 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0027', d: 'm 320 300 L 380 300 L 320 445 z', fill: '#393939'},
+       {id: '#path0028', d: 'm 320 300 L 380 200 L 380 300 z', fill: '#393939'},
+       {id: '#path0029', d: 'm 380 200 L 420 200 L 380 300 z', fill: '#393939'},
+       {id: '#path0030', d: 'm 420 200 L 450 240 L 390 240 z', fill: '#393939'},
+       {id: '#path0031', d: 'm 450 200 L 450 240 L 390 200 z', fill: '#393939'},
+       {id: '#path4604', d: 'm 52.987578,37.012422 0,-14.012422 14.012422,14.012422 z', fill: '#eec16e'},
+       {id: '#path5200', d: 'm 67,23 0,14.1 -14.1,-14.1 z', fill: '#eec16e'},
+ 
+     ],
+    ];
+    
+    var ts = [];
+    var curIndex = 0;
+    var timeId = null;
+    var slideTimeout = 7000;
+    
+    var imageTexts = [
+      "<b>West Virginia University</b> <i>Industrial Engineering</i> graduate from WVU May 2014. Wild and Wonderful. My hometown and alma mater.",
+      "<b>Deloitte Consulting</b> introduced me to <i>technology consulting</i> and client management, both integral to my professional and personal growth.",
+      "<b>3</b> introduced me to <i>technology consulting</i> and client management, both integral professional and personal growth.",
+      "<b>4</b> introduced me to <i>technology consulting</i> and client management, both integral professional and personal growth.",
+      "<b>5</b> introduced me to <i>technology consulting</i> and client management, both integral professional and personal growth."
+    ];
+    
+    var imageTitles = ["WVU", "Technology Consulting", "3", "4", "5"];
+    
+    function showText(index) {
+      // var slashText = imageTexts[index].replace(/[a-zA-Z0-9]/g, "/");
+      var element = document.querySelector(".image-text__body");
+      element.innerHTML = imageTexts[index];
+      document.querySelector(".image-text__title").textContent = imageTitles[index];
+    }
+    
+    function show(index, withDelay) {
+      ts[index].length = 0;
+      images[(index + 4) % 5 + 1].forEach(function(path, i) {
+        var delay = withDelay ? 10 * i : 0;
+        ts[index].push(
+          new anime({
+            targets: path.id,
+            d: {
+              value: path.d,
+              duration: 1000,
+              easing: "easeInOutQuad"
+            },
+            fill: {
+              value: path.fill,
+              duration: 1000,
+              easing: "easeInOutQuad"
+            },
+            delay: delay
+          })
+        );
+      });
+      showText(index);
+    }
+    
+    function play(index) {
+      ts[index].forEach(function(a) {
+        a.play();
+      });
+    }
+    
+    var svg = document.querySelector(".images");
+    var count = images[0].length;
+    
+    images[0].forEach(function(path, index) {
+      var newPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      svg.append(newPath);
+      newPath.setAttribute("id", path.id);
+      newPath.setAttribute("d", path.d);
+      newPath.setAttribute("style", "fill:" + path.fill);
+    });
+    
+    ts.push([]);
+    ts.push([]);
+    ts.push([]);
+    ts.push([]);
+    ts.push([]);
+    
+    setTimeout(() => {
+      document.querySelector(".image-text__body").innerHTML = imageTexts[0];
+      document.querySelector(".image-text__title").textContent = imageTitles[0];
+    }, 200);
+    
+    function prev() {
+      curIndex--;
+      if (curIndex < 0) curIndex = 4;
+    }
+    
+    function next() {
+      curIndex++;
+      curIndex %= 5;
+    }
+    
+    function resetAnimations(index) {
+      ts.forEach(function(t) {
+        if (t)
+          t.forEach(function(a) {
+            a.pause();
+          });
+      });
+      show(index, false);
+      play(index);
+      curIndex = index;
+      clearTimeout(timeId);
+      timeId = setTimeout(slideshow, slideTimeout);
+    }
+    
+    function restartAnimation(index) {
+      resetAnimations(index);
+    }
+    
+    function restartAnimationPrev() {
+      prev();
+      resetAnimations(curIndex);
+    }
+    
+    function restartAnimationNext() {
+      next();
+      resetAnimations(curIndex);
+    }
+    
+    document.addEventListener(
+      "keydown",
+      function(event) {
+        if (event.keyCode >= 1 && event.keyCode <= 100) {
+          restartAnimation(event.keyCode - 49);
+        } else if (event.keyCode === 37) {
+          // left
+          restartAnimationPrev();
+        } else if (event.keyCode === 39) {
+          // right
+          restartAnimationNext();
+        }
+      },
+      false
+    );
+    
+    function advance() {
+      next();
+      show(curIndex, true);
+      play(curIndex);
+    }
+    
+    function goTo(index) {
+      restartAnimation(index);
+    }
+    
+    function slideshow() {
+      advance();
+      timeId = setTimeout(slideshow, slideTimeout);
+    }
+    timeId = setTimeout(slideshow, slideTimeout);
